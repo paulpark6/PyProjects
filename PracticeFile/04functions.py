0a9323b3fd1948b7b2f573e76bdd61ed1969ff57
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-08-06 15:58:15 -0600

    05 loops and iterations

--- PracticeFile/04functions.py
+++ PracticeFile/04functions.py
@@ -6,6 +6,8 @@ def thing():
 thing()
 
 # max fuction -> this case find the lexiographically biggest one
+# Arguments -> thing you pass to your function.
+# Parameters -> variable which we use in the funtion definition
 big  = max("hello world z")
 
 print(big)
@@ -15,3 +17,6 @@ str_num = "123"
 num = int(str_num)
 num += 10
 print(num);
+
+
+
---
+++ PracticeFile/05loops_iteration.py
@@ -0,0 +1,41 @@
+#loop and iterations
+
+print(0 is 0.0)
+
+# while loops are indefinite loops -> it keeps going until statement is false
+n = 0
+while True:
+    n = input('Input the number of iterations: ')
+    try:  # if this causes error move to except
+        # this will cause an error if input is a string literal not string_integer
+        n = int(n)
+        break
+    except:  # when input was a string not integer
+        print('type in a integer!')
+
+# definite loops -> for keyword
+for i in range(n):
+    print(i)
+
+for i in [5,4,3,2,1]:
+    print(i)
+
+friends = ['Joseph', 'Glenn', 'Sally']
+for friend in friends:
+    print(friend)
+
+#"is" and "is not" operators
+print("helo" is not "bye"); # will print true
+print("helo" is "bye"); # will print false
+# finding the smallest value
+min = None # none is a special type for saying nothing
+data = [5,2,8,14,87,21,1,2,400]
+for value in data:
+    if min is None or min > value:
+        min = value;
+print('the smallest is ', min)
+
+# scope of python is weird... still can access last wtf....
+for stuff in data:
+    last = stuff
+print(last)
