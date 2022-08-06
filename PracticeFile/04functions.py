065534b93685812bdcfa6783e3c5248b7bec6ee1
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-08-06 14:17:09 -0600

    01 functions

--- PracticeFile/04functions.py
+++ PracticeFile/04functions.py
@@ -4,3 +4,14 @@ def thing():
     print("get lost")
     
 thing()
+
+# max fuction -> this case find the lexiographically biggest one
+big  = max("hello world z")
+
+print(big)
+
+# conversions
+str_num = "123"
+num = int(str_num)
+num += 10
+print(num);
