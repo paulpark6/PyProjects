ff36faac2a2e5e503686c6806bf68844065ea486
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-08-08 18:56:09 -0600

    Update readFile.py

--- PracticeFile/readFile.py
+++ PracticeFile/readFile.py
@@ -1,6 +1,7 @@
 
 ### function takes in data that was read from a txt file and replaces all space with '_'
-def underScore(data): 
+### function will dead with char
+def underScore_char(data): 
     new_data= ''
     for char in data:
         if char == ' ':
@@ -8,10 +9,18 @@ def underScore(data):
         new_data += char
     return new_data
 
+### function takes in data that was read from a txt file and replaces all space with '_'
+### function will dead with lines
+def underScore_lines(data):
+    new_data= ''
+    for lines in data:
+        new_data += lines.replace(' ', '_')
+    return new_data
 
 data = open("C:\GitHub-Projects\PyProjects\PracticeFile\\test.txt")
 text = data.read()
-new_data = underScore(text)
+#new_data = underScore_char(text)
+new_data = underScore_lines(text)
 print('the original data was:\n' , text , 
 '\n', 'the new data is: \n' , new_data)
 
