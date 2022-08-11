d3811b55fc0b3c80a8b8fffd3314cae6de6e9c7d
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-08-10 19:09:27 -0600

    reg ex update

--- PracticeFile/11RegEx.py
+++ PracticeFile/11RegEx.py
@@ -10,3 +10,10 @@ for line in file:
     line  = line.strip()
     if re.search('and', line):
         print(line)
+
+
+x = 'finding matching things liQke 1 or 30 of 3'
+y = re.findall('[0-9]+', x) # find all 0~9 digit or above
+print(y)
+y = re.findall('[AEFK]+',x) # find all upper case A E F K
+print(y)
